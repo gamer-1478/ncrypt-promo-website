86c2677dbefee9208b78ba5c0a66149a774b767d
Aayush Garg <aayushgarg2006@gmail.com>
2022-12-15 00:26:43 +0530

    Schema

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -1,1 +1,18 @@
 
+const mongoose = require('mongoose'),
+    reqString = { type: String, required: true },
+    moment = require('moment'),
+    now = new Date(),
+    dateStringWithTime = moment(now).format('YYYY-MM-DD HH:MM:SS');
+
+const userSchema = new mongoose.Schema({
+    email: reqString,
+    password: reqString,
+    date: {
+        type: String,
+        default: dateStringWithTime
+    },
+    orders: []
+})
+
+module.exports = mongoose.model("User", userSchema)
