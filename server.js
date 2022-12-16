51412cdc86ce4408edf4532768cbe78dc3109f9b
Aayush Garg <aayushgarg2006@gmail.com>
2022-12-16 23:30:21 +0530

    ok

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: (origin, callback) => callback(null, true),
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     withCredentials: true,
     credentials: true
